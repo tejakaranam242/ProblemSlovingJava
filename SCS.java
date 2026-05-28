02d82a68b3969f422e86635c0da6104fe7edf804
tejakaranam242 <karanamteja242@gmail.com>
2026-05-28 22:15:17 +0530

    SCS

---
+++ SCS.java
@@ -0,0 +1,16 @@
+public class SCS {
+    public static int minSuperSeq(String s1, String s2) {
+        int n1 = s1.length();
+        int n2 = s2.length();
+        int[][] dp = new int[n1+1][n2+1];
+        for(int i=0;i<=n1;i++) dp[i][0] = 0;
+        for(int j=0;j<=n2;j++) dp[0][j] = 0;
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return n1+n2-dp[n1][n2];
+    }
+}
