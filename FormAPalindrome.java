0ca349f9fbb468f042b06641a96b5d11fbed2ae6
tejakaranam242 <karanamteja242@gmail.com>
2026-05-28 22:18:37 +0530

    FormPali

---
+++ FormAPalindrome.java
@@ -0,0 +1,29 @@
+public class FormAPalindrome {
+    static String reverse(String s){
+        char[] rev = s.toCharArray();
+        int n = s.length()-1;
+        int m = 0;
+        while(m<n){
+            char temp = rev[m];
+            rev[m] = rev[n];
+            rev[n] = temp;
+            m++;
+            n--;
+        }
+        return new String(rev);
+    }
+    int findMinInsertions(String s) {
+        int n = s.length();
+        String rev = reverse(s);
+        int[][] dp = new int[n+1][n+1];
+        for(int i=0;i<=n;i++) dp[i][0] = 0;
+        for(int j=0;j<=n;j++) dp[0][j] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(s.charAt(i-1)==rev.charAt(j-1)) dp[i][j] = 1+dp[i-1][j-1];
+                else dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return n-dp[n][n];
+    }
+}
